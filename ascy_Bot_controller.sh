a50825806050e42070f4de91cb27cab9ba46f820
Paul Coste <paul@lescoste.net>
2022-01-30 18:35:10 +0100

    improves the controller script

--- ascy_Bot_controller.sh
+++ ascy_Bot_controller.sh
@@ -1,16 +1,59 @@
 #!/bin/bash
-
 botName="ASCyRespond"
 
-if [ $1 = "start" ]
-then
-	source ./env/bin/activate
-	./env/bin/python3 ./$botName/main.py 2> ./$botName/lastOutputASCy.txt &
-	echo $! > ./$botName/pidASCy.txt
-fi
+start() {
+	if [ -f ./$botName/pidASCy.txt ]
+    then
+        echo
+        echo "Already started. PID: [$( cat ./$botName/pidASCy.txt )]"
+    else
+        source ./env/bin/activate
+		./env/bin/python3 ./$botName/main.py 2> ./$botName/lastOutputASCy.txt &
+		echo $! > ./$botName/pidASCy.txt
+        fi
+    fi
+}
 
-if [ $1 = "stop" ]
-then
+status() {
+	echo
+    echo "==== Status"
+    if [ -f ./$botName/pidASCy.txt ]
+    then
+        echo
+        echo "Pid file: $( cat ./$botName/pidASCy.txt ) [./$botName/pidASCy.txt]"
+        echo
+        ps -ef | grep -v grep | grep $( cat ./$botName/pidASCy.txt )
+    else
+        echo
+        echo "No Pid file"
+    fi
+}
+
+stop() {
 	kill $(< ./$botName/pidASCy.txt)
+	rm ./$botName/pidASCy.txt
+}
 
-fi
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; sleep 1 ;
+            start
+            ;;
+    'status')
+            status
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
+
+exit 0
